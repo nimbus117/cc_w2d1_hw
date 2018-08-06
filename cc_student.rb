254c3dcbc37bdd38552a219cecf73cc22c2deefa
nimbus117 <mail@nimbus117.co.uk>
2018-08-06 15:06:38 +0100

    passing setter tests

--- cc_student.rb
+++ cc_student.rb
@@ -13,4 +13,12 @@ class CCStudent
   def cohort()
     return @cohort
   end
+
+  def set_name(new_name)
+    @name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
+  end
 end
