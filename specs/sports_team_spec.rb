a2358b53be1be0608669fb82f940740afe3879e2
nimbus117 <mail@nimbus117.co.uk>
2018-08-06 15:20:42 +0100

    created getter tests

--- specs/sports_team_spec.rb
+++ specs/sports_team_spec.rb
@@ -2,7 +2,20 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../sports_team.rb')
 
-class SportsTeam < Minitest::Test
+class SportsTeamTest < Minitest::Test
 
+  def test_get_team_name
+    team = SportsTeam.new('Falcons', ['Bob','Sally','Jim'], 'Wilma')
+    assert_equal('Falcons', team.name)
+  end
 
+  def test_get_team_players
+    team = SportsTeam.new('Falcons', ['Bob','Sally','Jim'], 'Wilma')
+    assert_equal(['Bob','Sally','Jim'], team.players)
+  end
+
+  def test_get_team_coach
+    team = SportsTeam.new('Falcons', ['Bob','Sally','Jim'], 'Wilma')
+    assert_equal('Wilma', team.coach)
+  end
 end
