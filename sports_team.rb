c402d4b9cdf72a6f3a24567e19ff8f8304ae9994
nimbus117 <mail@nimbus117.co.uk>
2018-08-06 15:31:00 +0100

    refactored using attr

--- sports_team.rb
+++ sports_team.rb
@@ -1,25 +1,28 @@
 
 class SportsTeam
 
+  attr_reader(:name, :players, :coach)
+  attr_writer(:coach)
+
   def initialize(name, players, coach)
     @name = name
     @players = players
     @coach = coach
   end
 
-  def name
-    return @name
-  end
-
-  def players
-    return @players
-  end
-
-  def coach
-    return @coach
-  end
-
-  def coach=(new_name)
-    @coach = new_name
-  end
+#  def name
+#    return @name
+#  end
+#
+#  def players
+#    return @players
+#  end
+#
+#  def coach
+#    return @coach
+#  end
+#
+#  def coach=(new_name)
+#    @coach = new_name
+#  end
 end
