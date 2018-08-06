fc5d838ed93b928422645be54a27ac7257f6477b
nimbus117 <mail@nimbus117.co.uk>
2018-08-06 14:57:58 +0100

    Initial commit

---
+++ specs/cc_student_spec.rb
@@ -0,0 +1,17 @@
+require(minitest/autorun)
+require(minitest/rg)
+require_relative(../cc_student.rb)
+
+
+class StudentTest < Minitest:: Test
+
+  def test_get_student_name
+    student = CCStudent.new('James', 'E24')
+    assert_equal('James', student.name)
+  end
+
+  def test_get_student_cohort
+    student = CCStudent.new('James', 'E24')
+    assert_equal('E24', student.cohort)
+  end
+end
