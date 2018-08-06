d0afaa922959b130b559eaa0b53bc82167c92974
nimbus117 <mail@nimbus117.co.uk>
2018-08-06 21:08:29 +0100

    passed get_book_by_title test

--- library.rb
+++ library.rb
@@ -5,4 +5,12 @@ class Library
   def initialize(books)
     @books = books
   end
+
+  def book_by_title(title)
+    for book in @books
+      if book[:title] == title
+        return book
+      end
+    end
+  end
 end
